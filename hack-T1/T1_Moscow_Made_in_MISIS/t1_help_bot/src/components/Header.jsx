ef0725d8cf93e4cf0167ca1d21c38ff33a6e84f4
Mikhail Chashin <42130525+vvvvtrt@users.noreply.github.com>
2024-11-27 16:44:47 +0300

    Add files via upload

---
+++ hack-T1/T1_Moscow_Made_in_MISIS/t1_help_bot/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './header.css'
+
+
+const Header = () => {
+
+    return(
+        <>
+        <div className="header">
+            <img src="./logo.svg"/>
+            <img src="./avatar.jpg"/>
+        </div>
+        </>
+    )
+}
+
+export default Header;
