d0abb382e640f67107c908a855f17568ac78629f
eureu <venekbespalov@mail.ru>
2024-11-27 17:37:11 +0300

    upd: frontend

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,14 @@
+import "./header.css";
+
+const Header = () => {
+  return (
+    <>
+      <div className="header">
+        <img src="./logo.svg" />
+        <img src="./avatar.jpg" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
